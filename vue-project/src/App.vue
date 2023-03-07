b8706a76970aa640825757ceb397c02d56c5467c
zabinskas.paulius951@gmail.com <zabinskas.paulius951@gmail.com>
2023-03-07 13:16:54 +0000

    Added navigation tab component, rough workings

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
+  <Navigation />
   <router-view></router-view>
 </template>
 
--- vue-project/src/router/routes.js
+++ vue-project/src/router/routes.js
@@ -1,14 +1,35 @@
 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "../.api/firebase";
+
+function isAuth(to, from, next) {
+    console.log("Checking auth");
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log(user);
+            // User is signed in so continue to desired page
+            return next();
+            // ...
+        } else {
+            // User is signed out
+            // Send them back to the home page or maybe the login page
+            return next({path: '/'});
+        }
+    });
+}
+
 function loadPage (component) {
-  // '@' is aliased to src/components
-  return () => import(/* webpackChunkName: "[request]" */
-  `@/views/${component}.vue`)}
+    // '@' is aliased to src/components
+    return () => import(/* webpackChunkName: "[request]" */ `@/views/${component}.vue`)
+}
 
   export default [
     { path: '/', component: loadPage('Home') },
     { path: '/blog', component: loadPage('Blog') },
     { path: '/about', component: loadPage('About') },
     { path: '/Register', component: loadPage('Register') },
-    { path: '/RegLecture', component: loadPage('RegLecture') }
+    { path: '/RegLecture', component: loadPage('RegLecture') },
+    { path: '/secure', component: loadPage('Secure')}
 
   ]
