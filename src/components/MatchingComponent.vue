83ea62f099fc649abe9c77dae8d7d10a5ab2d003
Chaalie <c.quinlan3@nuigalway.ie>
2023-03-29 23:38:52 +0100

    Update MatchingComponent.vue
    adds new box in match

--- src/components/MatchingComponent.vue
+++ src/components/MatchingComponent.vue
@@ -2,21 +2,27 @@
   <div class="mb-3 right">
     <button type="button" @click="getMatchingUsers" class="btn btn-primary">Show Matching Users</button>
   </div>
-  <div class="matching-users">
+
+  <div v-if="locationArray.length > 0">
     <table>
       <thead>
       <tr>
-        <th>User ID</th>
-        <th>Name</th>
-        <th>Email</th>
+        <th>Username</th>
+        <th>Origin</th>
+        <th>Destination</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="user in formattedUsers" :key="user.id">
-        <td>{{ user.id }}</td>
-        <td>{{ user.name }}</td>
-        <td>{{ user.email }}</td>
-      </tr>
+      <template v-for="(result, index) in locationArray">
+
+        <template v-if="index % 3 === 0">
+          <tr>
+            <td>{{ locationArray[index].username ? locationArray[index].username : 'Empty' }}</td>
+            <td v-if="locationArray[index + 1]">{{ locationArray[index + 1].data ? locationArray[index + 1].data.result : 'Empty' }}</td>
+            <td v-if="locationArray[index + 2]">{{ locationArray[index + 2].data ? locationArray[index + 2].data.result : 'Empty' }}</td>
+          </tr>
+        </template>
+      </template>
       </tbody>
     </table>
   </div>
@@ -45,7 +51,10 @@ export default {
   },
   methods: {
     async getMatchingUsers() {
+      console.log("Hello1wworld");
+      console.log(this.app);
       const functions = getFunctions(app);
+      console.log(functions)
       const getMatchingUsers = httpsCallable(functions, 'getmatchingusers');
       try {
         console.log(this.map1, this.map2);
@@ -54,20 +63,12 @@ export default {
           "destination": this.map2
         });
         console.log(result.data);
+        this.result = result.data;
         this.locationArray = result.data;
       } catch (error) {
         console.error(error);
       }
     }
-  },
-  computed: {
-    formattedUsers() {
-      return this.locationArray.map(user => ({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-      }));
-    }
-  },
-};
+  }
+}
 </script>
